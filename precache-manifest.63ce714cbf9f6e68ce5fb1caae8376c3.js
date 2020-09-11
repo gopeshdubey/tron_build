2872466507b9574cfc9555eb007a495b4e9d5f50
gopeshdubey <gdubey96.gd@gmail.com>
2020-09-11 23:00:19 +0530

    my

---
+++ precache-manifest.63ce714cbf9f6e68ce5fb1caae8376c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ec2c924b414c0630ce0374e8543094",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfe32baeb7577f6597df",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e7be4813e1825206539b",
+    "url": "/static/css/main.a7f76bd4.chunk.css"
+  },
+  {
+    "revision": "cfe32baeb7577f6597df",
+    "url": "/static/js/2.fdc8a7b8.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/static/js/2.fdc8a7b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7be4813e1825206539b",
+    "url": "/static/js/main.63e91eb5.chunk.js"
+  },
+  {
+    "revision": "c19176b25684d4dc219e",
+    "url": "/static/js/runtime-main.0fa989cd.js"
+  }
+]);
